3b5cb16668f9b730fe63fc238f20c779d777efa8
VR Labb Vive 2 <test@test.com>
2018-04-16 17:32:29 +0200

    blood splatter

---
+++ Assets/Blood Splatter Effect/Editor/Editor_BloodSplatterEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Editor_BloodSplatterEffect : Editor
+{
+    [MenuItem("Window/Blood Splatter Effect/Create")]
+    public static void CreateBloodSplatter()
+    {
+        GameObject template = (GameObject)Resources.Load("EffectTemplate");
+        if (template != null)
+        {
+            GameObject clone = Instantiate(template);
+            clone.name = "BloodSplatterEffect";
+            Selection.activeGameObject = clone;
+            Undo.RegisterCreatedObjectUndo(clone, "Created BloodSplatterEffect");
+        }
+        else
+        {
+            Debug.LogWarning("The blood splatter effect template could not be found! Consider re-importing the asset");
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerScript : MonoBehaviour {
+
+	public BloodSplatterEffect bloodEffect;
+
 	private int health;
 	// Use this for initialization
 	void Start () {
+		//bloodEffect = GetComponent<BloodSplatterEffect> ();
 		health = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		bloodEffect.BloodSplatter ();
 	}
+
 	public bool hurt(int damage){
 		health += damage;
 		return health <= 0;
